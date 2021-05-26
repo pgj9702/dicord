42d5935f0a7f16e11eb2c8ba5af8c9d0d35d39a1
pgj9702 <cao147147@naver.com>
2021-05-26 18:07:08 +0900

    test bot03

--- db_conn/heroku_db_conn.py
+++ db_conn/heroku_db_conn.py
@@ -6,6 +6,7 @@ from DB_SETTINGS import HEROKU_DB_SETTINGS
 
 heroku_db_settings = HEROKU_DB_SETTINGS
 
+# DB connect
 heroku_db = pymysql.connect(
     user=heroku_db_settings['user'],
     passwd=heroku_db_settings['passwd'],
