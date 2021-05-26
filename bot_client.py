499ddd1104d5baf468303c4fe6c6661aca079d2d
pgj9702 <cao147147@naver.com>
2021-05-26 18:04:01 +0900

    test bot03

--- bot_client.py
+++ bot_client.py
@@ -21,7 +21,26 @@ client = commands.Bot(command_prefix='*', intents=intents)
 async def hello(ctx):
     await ctx.send('안녕하세요')
     await ctx.send('안녕하세요 {}'.format(ctx.guild.owner))
+    await ctx.send('안녕하세요 {}'.format(ctx.author.name))
+    await ctx.author.send('안녕하세요 {}'.format(ctx.author.name)) # 개인 메시지
+
+    print(dir(ctx.author))
     print("hello")
+
+# ctx.author
+# '__abstractmethods__', '__class__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__',
+# '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__',
+# '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__slots__', '__str__',
+# '__subclasshook__', '_abc_impl', '_client_status', '_copy', '_from_message', '_from_presence_update', '_get_channel',
+# '_presence_update', '_roles', '_state', '_try_upgrade', '_update', '_update_from_message', '_update_inner_user',
+# '_update_roles', '_user', 'activities', 'activity', 'add_roles', 'avatar', 'avatar_url', 'avatar_url_as', 'ban',
+# 'block', 'bot', 'color', 'colour', 'create_dm', 'created_at', 'default_avatar', 'default_avatar_url',
+# 'desktop_status', 'discriminator', 'display_name', 'dm_channel', 'edit', 'fetch_message', 'guild',
+# 'guild_permissions', 'history', 'id', 'is_avatar_animated', 'is_blocked', 'is_friend', 'is_on_mobile', 'joined_at',
+# 'kick', 'mention', 'mentioned_in', 'mobile_status', 'move_to', 'mutual_friends', 'mutual_guilds', 'name', 'nick',
+# 'pending', 'permissions_in', 'pins', 'premium_since', 'profile', 'public_flags', 'raw_status', 'relationship',
+# 'remove_friend', 'remove_roles', 'request_to_speak', 'roles', 'send', 'send_friend_request', 'status', 'system',
+# 'top_role', 'trigger_typing', 'typing', 'unban', 'unblock', 'voice', 'web_status'
 
 # user's message event
 @client.event
@@ -36,6 +55,9 @@ async def on_message(message):
     if content == "!ping":
         await message.channel.send("Pong!")
 
+    await client.process_commands(message)
+
+
 # 타 함수에서 다른 bot 함수 사용
 # await bot.process_commands(msg)
 
@@ -46,12 +68,12 @@ async def on_message(message):
 # music, picture etc .. file upload
 
 # client 실행
-client.run(os.environ['token'])
+# client.run(os.environ['token'])
 
 # terminal test
-# from bot_token import JB_APP_TOKEN
-# bot_token = JB_APP_TOKEN
-# client.run(bot_token)
+from bot_token import JB_APP_TOKEN
+bot_token = JB_APP_TOKEN
+client.run(bot_token)
 
 
 
---
+++ db_conn/heroku_db_conn.py
@@ -0,0 +1,15 @@
+# mysql
+import pymysql
+
+# heroku db settings
+from DB_SETTINGS import HEROKU_DB_SETTINGS
+
+heroku_db_settings = HEROKU_DB_SETTINGS
+
+heroku_db = pymysql.connect(
+    user=heroku_db_settings['user'],
+    passwd=heroku_db_settings['passwd'],
+    host=heroku_db_settings['host'],
+    db=heroku_db_settings['db'],
+    charset='utf8'
+)
